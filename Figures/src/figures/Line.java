121a3ef9eab01b4688b42e68148c999dac6ad47b
Nacho <>
2018-09-08 09:34:20 -0600

    Creacion de las clases point, line, triangle y rectangle

--- Figures/src/figures/Line.java
+++ Figures/src/figures/Line.java
@@ -6,10 +6,42 @@
 package figures;
 
 /**
- * Clase para la linea con dos punto 
+ * Clase para la linea con dos puntos
  * @author Jose Zamora y Esteban Barrantes
  * @ version
  */
 public class Line {
-    
+   private Point pointStart; 
+   private Point pointEnd;
+
+    public Line() {
+    }
+
+    public Line(Point pointStart, Point pointEnd) {
+        this.pointStart = pointStart;
+        this.pointEnd = pointEnd;
+    }
+
+    public Point getPointStart() {
+        return pointStart;
+    }
+
+    public void setPointStart(Point pointStart) {
+        this.pointStart = pointStart;
+    }
+
+    public Point getPointEnd() {
+        return pointEnd;
+    }
+
+    public void setPointEnd(Point pointEnd) {
+        this.pointEnd = pointEnd;
+    }
+
+    @Override
+    public String toString() {
+        return "Line{" + "pointStart=" + pointStart + ", pointEnd=" + pointEnd + '}';
+    }
+   
+   
 }
--- Figures/src/figures/Point.java
+++ Figures/src/figures/Point.java
@@ -12,4 +12,36 @@ package figures;
  */
 public class Point {
     private int X;
+    private int Y;
+
+    public Point() {
+    }
+
+    public Point(int X, int Y) {
+        this.X = X;
+        this.Y = Y;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int X) {
+        this.X = X;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int Y) {
+        this.Y = Y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" + "X=" + X + ", Y=" + Y + '}';
+    }
+    
+    
 }

add6f36a1c957d834d38ad94389401e1a53178e6
EstebanBV01 <>
2018-09-08 15:03:54 -0600

    probando a ver si esta vez sirve

--- Figures/src/figures/Point.java
+++ Figures/src/figures/Point.java
@@ -14,9 +14,8 @@ public class Point {
     private int X;
     private int Y;
 
-    public Point() {
+    public Point( ) {
     }
-
     public Point(int X, int Y) {
         this.X = X;
         this.Y = Y;
